f5846f11f6b4e090d240b13c67a03506d96ce0b3
PraderioM <PraderioM@Gmail.com>
2025-10-19 12:15:43 +0200

    Download no longer freezing when not enough images are found

--- image_downloader.py
+++ image_downloader.py
@@ -1,11 +1,53 @@
-import os
 from glob import glob
+import os
+import urllib
+import re
 
 from bing_image_downloader import downloader
-from progressbar import progressbar
+from progressbar import progressbar, ProgressBar
 
 from input_request_tools import get_out_path, get_string, get_integer
 
+# The bing app is overwritten so that the process halts after a specified number of pages unable to download new images.
+def run(self, no_new_downloads_limit: int = 10, verbose: bool= True):
+    pages_without_new_downloads = 0
+    bar = ProgressBar(self.limit)
+    while (self.download_count < self.limit) and (pages_without_new_downloads < no_new_downloads_limit):
+        if verbose:
+            print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
+        # Parse the page source and download pics
+        request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
+                      + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
+                      + '&adlt=' + self.adult + '&qft=' + self.filters
+        request = urllib.request.Request(request_url, None, headers=self.headers)
+        response = urllib.request.urlopen(request)
+        html = response.read().decode('utf8')
+        links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
+
+        if verbose:
+            print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
+            print("\n===============================================\n")
+
+        prev_downloads = self.download_count
+        for link in links:
+            if self.download_count < self.limit:
+                self.download_image(link)
+                bar.update(self.download_count)
+            else:
+                if verbose:
+                    print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
+                    print("\n===============================================\n")
+                break
+
+        self.page_counter += 1
+        if prev_downloads == self.download_count:
+            pages_without_new_downloads += 1
+        else:
+            pages_without_new_downloads = 0
+
+    if self.download_count < self.limit:
+        if verbose:
+            print(f"Unable to find {self.limit} images for query \"{self.query}\"")
 
 def main():
     out_dir = get_out_path('Insert directory where you want to save the images', allow_duplicate=True)
@@ -29,13 +71,14 @@ def main():
     limit = get_integer(question='Insert maximum number of images you wish to download for each query', default=100, min_val=10)
 
     for query_str in progressbar(query_str_list):
+        downloader.run = run
         downloader.download(query_str,
                             limit=limit,
                             output_dir=out_dir,
                             adult_filter_off=True,
                             force_replace=False,
                             timeout=60,
-                            verbose=True)
+                            verbose=False)
 
 
 if __name__ == '__main__':
